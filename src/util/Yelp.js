416abb3baf556e07564244f0ebc2a11b18fa4821
Kaelin King <kaelin@kingadventure.com>
2020-11-05 18:35:21 -0500

    update zipcode error

---
+++ src/util/Yelp.js
@@ -0,0 +1,35 @@
+var apiKey = 'UiKvgVUX_Im5N315-v9ZVC5jF6RyJpFBhWfQy8KGpYARCh11V6ZN235-gudTKkCZo2lM6fudwZQC7vAqxy2gZ5dz1Z7TwMmWDsxD_LHqOvmg8wZDGAp8DBSzMmfWXnYx';
+
+var Yelp = {
+
+search(term, location, sortBy){
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+  {headers: {
+    Authorization: `Bearer ${apiKey}`
+  }}).then(
+    (response) => {
+      return response.json()
+    }
+  ).then(
+    (jsonResponse) => {
+      if(jsonResponse.businesses){
+        return jsonResponse.businesses.map(((business) => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          };
+        }));
+      }
+    }
+  );
+}}
+
+export default Yelp;
